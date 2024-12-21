9dc82ecee04c128d8087cc04e5a804c735c0e16f
The Wizard of Hahz <info@luvnft.com>
2024-12-20 19:27:51 -0500

    up

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,12 +3,12 @@ export default function NavBar() {
     return (
     <div className="navbar bg-base-100 fixed z-10 shadow-xl">
         <div className="flex-1">
-            <Link href="/" className="btn btn-ghost text-xl">OpenPods.org</Link>
+            <Link href="/" className="btn btn-ghost text-xl">POD</Link>
         </div>
         <div className="flex-none">
             <ul className="menu menu-horizontal px-1 gap-3">
-                <li><Link href="/search" className="btn bg-primary text-white">Search</Link></li>
-                <li><Link className="btn bg-secondary text-white" href="https://github.com/haydenzeller/openpods.org/" target="_blank">Contribute</Link></li>
+                <li><Link href="/search" className="btn bg-primary text-white">SEARCH</Link></li>
+                <li><Link className="btn bg-secondary text-white" href="https://t.me/atl5d" target="_blank">T.ME/ATL5D</Link></li>
             </ul>
         </div>
     </div>
